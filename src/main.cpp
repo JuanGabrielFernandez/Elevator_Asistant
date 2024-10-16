96596d5577bafe331a36c1b1483a0e6178c39d66
JuanGabrielFernandez <juanfernandez0011@gmail.com>
2024-10-16 21:26:48 +0200

    Cambio a control de servos por wifi mqtt con tecla '5'

--- src/main.cpp
+++ src/main.cpp
@@ -1,63 +1,129 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
 #include <Arduino.h>
 #include "ESP32_Servo.h"
-#include <WiFi.h>
-#include "BluetoothSerial.h"
 
-String device_name = "ESP32-BT-Slave";
-// Check if Bluetooth is available
-#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
-#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
-#endif
+WiFiClient esp32Client;
+PubSubClient mqttClient(esp32Client);
 
-// Check Serial Port Profile
-#if !defined(CONFIG_BT_SPP_ENABLED)
-#error Serial Port Profile for Bluetooth is not available or not enabled. It is only available for the ESP32 chip.
-#endif
+const char *ssid = "WiFi_OliveNet-F7BFAC";
+const char *password = "X4HmTsqD";
 
-BluetoothSerial SerialBT;
+char *server = "test.mosquitto.org";
+int port = 1883;
 
 Servo miServo;
 
 const int pinServo = 18;
 
+int var = 0;
+char datos[40];
+String resultS = "";
+
+void wifiInit()
+{
+  Serial.print("Conectándose a ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println("");
+  Serial.println("Conectado a WiFi");
+  Serial.println("Dirección IP: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char *topic, byte *payload, unsigned int length)
+{
+  Serial.print("Mensaje recibido [");
+  Serial.print(topic);
+  Serial.print("] ");
+
+  char payload_string[length + 1];
+
+  int resultI;
+
+  memcpy(payload_string, payload, length);
+  payload_string[length] = '\0';
+  resultI = atoi(payload_string);
+
+  var = resultI;
+
+  resultS = "";
+
+  for (int i = 0; i < length; i++)
+  {
+    resultS = resultS + (char)payload[i];
+  }
+  Serial.println();
+}
+
+void reconnect()
+{
+  while (!mqttClient.connected())
+  {
+    Serial.print("Intentando conectarse MQTT...");
+
+    if (mqttClient.connect("arduinoClient"))
+    {
+      Serial.println("Conectado");
+
+      mqttClient.subscribe("Entrada/01");
+    }
+    else
+    {
+      Serial.print("Fallo, rc=");
+      Serial.print(mqttClient.state());
+      Serial.println(" intentar de nuevo en 5 segundos");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
 void setup()
 {
+
   Serial.begin(115200);
-  SerialBT.begin(device_name); // Bluetooth device name
-  // SerialBT.deleteAllBondedDevices(); // Uncomment this to delete paired devices; Must be called after begin
-  Serial.printf("The device with name \"%s\" is started.\nNow you can pair it with Bluetooth!\n", device_name.c_str());
-  miServo.attach(pinServo);
+  delay(10);
+  wifiInit();
+  mqttClient.setServer(server, port);
+  mqttClient.setCallback(callback);
 }
 
 void loop()
 {
-  if (Serial.available())
+  if (!mqttClient.connected())
   {
-    SerialBT.write(Serial.read());
+    reconnect();
   }
-  if (SerialBT.available())
+  mqttClient.loop();
+
+  
+
+  if (var == 5)
   {
-    char data = ((byte)SerialBT.read());
-    switch (data)
-    {
-    case 'O':
-      miServo.attach(pinServo);
-      miServo.write(0); // Mover el servo a 0 grados
-      delay(1000);
-      miServo.detach();
-      delay(1000);
-      miServo.attach(pinServo); // Esperar 1 segundo
-      miServo.write(180);       // Mover el servo a 90 grados
-      delay(1000);              // Esperar 1 segundo
-      miServo.detach();
-      break;
+    Serial.print("Activo servo \n");
+    Serial.print("String: ");
+    Serial.println(resultS);
 
-    default:
-      break;
-    }
-    Serial.println(data);
+    miServo.attach(pinServo);
+    miServo.write(0); // Mover el servo a 0 grados
+    delay(1000);
+    miServo.detach();
+    delay(1000);
+    miServo.attach(pinServo); // Esperar 1 segundo
+    miServo.write(180);       // Mover el servo a 90 grados
+    delay(1000);              // Esperar 1 segundo
+    miServo.detach();
+
+    var = 0;// Inicializo la variable que envie por mqtt para que no entre al if todo el rato
   }
-
-  delay(20);
-  // put your main code here, to run repeatedly:ç
+  
+  delay(5000);
 }
