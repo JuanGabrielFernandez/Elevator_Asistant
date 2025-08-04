39df56e8e283d728d07fc83d6d4fa2e01c1c60b4
JuanGabrielFernandez <juanfernandez0011@gmail.com>
2025-08-04 17:49:21 +0200

    micro-ROS code

--- src/main.cpp
+++ src/main.cpp
@@ -1,130 +1,71 @@
-/*
-  Basic ESP32 MQTT example
-  This sketch demonstrates the capabilities of the pubsub library in combination
-  with the ESP32 board/library.
-  It connects to an MQTT server then:
-  - publishes "connected to MQTT" to the topic "outTopic"
-  - subscribes to the topic "inTopic", printing out messages
-    it receives. NB - it assumes the received payloads are strings not binary
-  - If the "ON" payload is send to the topic "inTopic" , LED will be turned on, and acknowledgement will be send to Topic "outTopic"
-  - If the "OFF" payload is send to the topic "inTopic" , LED will be turned OFF, and acknowledgement will be send to Topic "outTopic"
-  It will reconnect to the server if the connection is lost using a
-  reconnect function.
-*/
 
-#include <WiFi.h>
-#include <PubSubClient.h>
-#include <Arduino.h>
-#include "ESP32_Servo.h"
+CODIGO POR UDP
+#include <micro_ros_arduino.h>
+#include <ESP32_Servo.h>
+#include <WiFi.h>  // Para conectarse a WiFi
 
-const char* mqtt_server = "test.mosquitto.org"; //mqtt server
-const char* ssid = "WiFi_OliveNet-F7BFAC";
-const char* password = "X4HmTsqD";
+#include <rcl/rcl.h>
+#include <rclc/rclc.h>
+#include <rclc/executor.h>
+#include <std_msgs/msg/int32.h>
 
-WiFiClient espClient;
-PubSubClient client(espClient); //lib required for mqtt
+#define pinServo 18
+
+rcl_subscription_t subscriber;
+std_msgs__msg__Int32 msg_sub;
+
+rclc_executor_t executor;
+rclc_support_t support;
+rcl_allocator_t allocator;
+rcl_node_t node;
 
 Servo miServo;
-const int pinServo = 18;
 
-void callback(char* topic, byte* payload, unsigned int length) {   //callback includes topic and payload ( from which (topic) the payload is comming)
-  Serial.print("Message arrived [");
-  Serial.print(topic);
-  Serial.print("] ");
-  for (int i = 0; i < length; i++)
-  {
-    Serial.print((char)payload[i]);
-  }
-  if ((char)payload[0] == 'O' && (char)payload[1] == 'N') //on
-  {
-    Serial.println("on");
-    client.publish("SALIDA/01", "SERVO turned ON");
+#define RCCHECK(fn) { rcl_ret_t rc = fn; if(rc != RCL_RET_OK) {while(1);}}
+#define RCSOFTCHECK(fn) { rcl_ret_t rc = fn; if(rc != RCL_RET_OK) {}}
+
+void subscription_callback(const void * msgin)
+{
+  const std_msgs__msg__Int32 * msg = (const std_msgs__msg__Int32 *)msgin;
+
+  if(msg->data == 0) {
     miServo.attach(pinServo);
-    miServo.write(0); // Mover el servo a 0 grados
-    delay(1000);
-    miServo.detach();
-    delay(1000);
-    miServo.attach(pinServo); // Esperar 1 segundo
-    miServo.write(180);       // Mover el servo a 90 grados
-    delay(1000);              // Esperar 1 segundo
+    miServo.writeMicroseconds(750);
+    delay(1250);
     miServo.detach();
 
-  }
-  else if ((char)payload[0] == 'O' && (char)payload[1] == 'F' && (char)payload[2] == 'F') //off
-  {
-
-  }
-  Serial.println();
-}
-void reconnect() {
-  while (!client.connected()) {
-    Serial.println("Attempting MQTT connection...");
-    if (client.connect("ESP32_clientID")) {
-      Serial.println("connected");
-      // Once connected, publish an announcement...
-      client.publish("SALIDA/01", "Nodemcu connected to MQTT");
-      // ... and resubscribe
-      client.subscribe("Entrada/01");
-
-    } else {
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
-      delay(5000);
-    }
+    miServo.attach(pinServo);
+    miServo.writeMicroseconds(1500);
+    delay(100);
+    miServo.detach();
   }
 }
-void connectmqtt()
-{
-  client.connect("ESP32_clientID");  // ESP will connect to mqtt broker with clientID
-  {
-    Serial.println("connected to MQTT");
-    // Once connected, publish an announcement...
 
-    // ... and resubscribe
-    client.subscribe("Entrada/01"); //topic=Demo
-    client.publish("SALIDA/01",  "connected to MQTT");
+void setup() {
+  // Configurar transporte UDPset_microros_udp_transports
+  set_microros_wifi_transports((char*)"ssid", (char*)"pass", (char*)"ip", 8888u);
 
-    if (!client.connected())
-    {
-      reconnect();
-    }
-  }
-}
-void setup()
-{
-  Serial.begin(115200);
-    Serial.print("Conectándose a ");
-  Serial.println(ssid);
+  delay(2000);
 
-  WiFi.begin(ssid, password);
 
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    Serial.print(".");
-    delay(500);
-  }
-  Serial.println("");
-  Serial.println("Conectado a WiFi");
-  Serial.println("Dirección IP: ");
-  Serial.println(WiFi.localIP());
+  allocator = rcl_get_default_allocator();
+  RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
+    // create node
+  RCCHECK(rclc_node_init_default(&node, "micro_ros_arduino_wifi_node", "", &support));
+  
+  RCCHECK(rclc_subscription_init_default(
+    &subscriber,
+    &node,
+    ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
+    "elevator"));
 
-  Serial.println("connected");
-  client.setServer(mqtt_server, 1883);//connecting to mqtt server
-  client.setCallback(callback);
-  //delay(5000);
-  connectmqtt();
+  RCCHECK(rclc_executor_init(&executor, &support.context, 1, &allocator));
+  RCCHECK(rclc_executor_add_subscription(&executor, &subscriber, &msg_sub, &subscription_callback, ON_NEW_DATA));
 }
 
-
-void loop()
-{
-  // put your main code here, to run repeatedly:
-  if (!client.connected())
-  {
-    reconnect();
-  }
-
-  client.loop();
+void loop() {
+  Serial.println("cargando...");
+  RCCHECK(rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100)));
+  delay(100);
 }
+
